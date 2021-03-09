4adb9bcdc641344605c7085ae07280e65635e801
Lucas Falaschini <lucasfalaschini@gmail.com>
2021-03-09 17:22:04 -0300

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  url: 'http://api.openweathermap.org/data/2.5/weather?appid=',
-  key: '2d775446b1f8702fc4d41e604afdc8a9',
-  urlConfig: '&units=metric&lang=es&q='
-};
